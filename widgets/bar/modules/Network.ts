4eb265c218452f798cdabd90d68cd48a2d632d63
ImRayy <souradeep_roy@pm.me>
2024-10-26 15:05:01 +0530

    feat(network): add network up/down speed

--- widgets/bar/modules/Network.ts
+++ widgets/bar/modules/Network.ts
@@ -1,13 +1,20 @@
+import { getNetworkSpeed as speed } from "module-vars/net-speed";
 const network = await Service.import("network");
 
 const NetworkIndicator = () =>
   Widget.Box({
+    spacing: 4,
+    class_name: "network-indicator",
     children: [
       Widget.Icon().hook(network, (self) => {
         const icon = network[network.primary || "wired"]?.icon_name;
         self.icon = icon || "";
         self.visible = !!icon;
-        self.class_name = "network-indicator";
+      }),
+      Widget.Label({ class_name: "value" }).hook(speed, (self) => {
+        const d = Number.parseInt(speed.value.in);
+        const o = Number.parseInt(speed.value.out);
+        self.label = d > o ? `${speed.value.in} 󰦿` : `${speed.value.out} 󰧇`;
       }),
     ],
   });
