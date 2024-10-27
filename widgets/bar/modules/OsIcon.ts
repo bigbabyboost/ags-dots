e6b62ffd3212722602eba2000e94760b296372dc
ImRayy <souradeep_roy@pm.me>
2024-10-27 21:22:36 +0530

    feat(OsIcon): make os/distro icons dynamic

--- widgets/bar/modules/OsIcon.ts
+++ widgets/bar/modules/OsIcon.ts
@@ -1,7 +1,28 @@
+import { distroName } from "module-vars/sysinfo";
+
+const getIcons = (distroName: string) => {
+  switch (distroName) {
+    case "fedora":
+      return "";
+    case "arch":
+      return " ";
+    case "nixos":
+      return "";
+    case "debian":
+      return "";
+    case "opensuse-tumbleweed":
+      return "";
+    case "endeavouros":
+      return "";
+    default:
+      return "";
+  }
+};
+
 export default Widget.EventBox({
   class_name: "distro-icon",
   onPrimaryClick: () => App.toggleWindow("quicksettings"),
   child: Widget.Label({
-    label: "",
+    label: getIcons(distroName().toLowerCase()),
   }),
 });
