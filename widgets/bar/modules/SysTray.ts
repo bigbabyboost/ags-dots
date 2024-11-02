8c93b32cdd4793c157980f6605d0a35b31b438f5
ImRayy <souradeep_roy@pm.me>
2024-11-02 10:56:52 +0530

    style: update SysTray

--- widgets/bar/modules/SysTray.ts
+++ widgets/bar/modules/SysTray.ts
@@ -1,23 +1,39 @@
+import type { TrayItem } from "types/service/systemtray";
 import Separator from "./Separator.ts";
+import GdkPixbuf from "types/@girs/gdkpixbuf-2.0/gdkpixbuf-2.0";
 
 const systemtray = await Service.import("systemtray");
+const ignoreList = ["KDE Connect Indicator"];
+
+const SysTrayItem = (item: TrayItem) =>
+  Widget.Button({
+    class_name: "systray-icon",
+    css: "padding: 0 4px;",
+    // child: Widget.Icon({ icon: item.bind("icon") }),
+    child: Widget.Icon().hook(systemtray, (self) => {
+      if (item.title === "TelegramDesktop") {
+        self.icon = "org.telegram.desktop";
+        self.class_name = "systray-icon custom";
+      } else {
+        self.icon = item.icon;
+        self.class_name = "systemtray-icon";
+      }
+    }),
+    on_primary_click: (_, event) => item.activate(event),
+    on_secondary_click: (_, event) => item.openMenu(event),
+    tooltip_markup: item.bind("tooltip_markup"),
+  });
 
 export default () =>
   Widget.Box({
     class_name: "systray",
     setup: (self) =>
       self.hook(systemtray, () => {
-        self.children = systemtray.items.map((item) =>
-          Widget.Button({
-            class_name: "systray-icon",
-            css: "padding: 0 4px;",
-            child: Widget.Icon({ icon: item.bind("icon") }),
-            on_primary_click: (_, event) => item.activate(event),
-            on_secondary_click: (_, event) => item.openMenu(event),
-            tooltip_markup: item.bind("tooltip_markup"),
-          }),
-        );
-        if (systemtray.bind("items").emitter.items.length > 0) {
+        self.children = systemtray.items
+          .filter(({ id }) => id && !ignoreList.includes(id))
+          .map(SysTrayItem);
+
+        if (systemtray.items.length > 0) {
           self.add(Separator());
         }
 
